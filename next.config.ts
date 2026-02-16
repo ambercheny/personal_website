ed15478fd1d446323743af3e9a3d0a89082cf68e
ambercheny <end1859612@gmail.com>
2026-02-15 16:40:20 -0800

    revise next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export', // Required for static hosting like GitHub Pages
-  
-  // Replace 'personal_website' with your repository name on GitHub
-  basePath: '/personal_website', 
-  
+  output: 'export',
   images: {
-    // GitHub Pages is a static host and cannot process Next.js image optimization
-    unoptimized: true,
+    unoptimized: true, // Required for static export
   },
 };
 
